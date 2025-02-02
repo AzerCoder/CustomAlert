e90f4f67a4a0c799b8aff8f5726e284a83472e19
A'zamjon Abdumuxtorov <azamjonabdumuxtorov1220@gmail.com>
2025-02-02 23:21:04 +0500

    Initial Commit

---
+++ CustomAlert/CustomAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomAlertApp.swift
+//  CustomAlert
+//
+//  Created by A'zamjon Abdumuxtorov on 02/02/25.
+//
+
+import SwiftUI
+
+@main
+struct CustomAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
